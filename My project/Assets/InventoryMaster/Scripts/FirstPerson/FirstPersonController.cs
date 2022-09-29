bbf37c4d57d0267f74cba834dcc292e04ee3f4f9
MrDevli <goida.dmytro@gmail.com>
2022-09-29 11:34:45 +0300

    фіфвфв

--- My project/Assets/InventoryMaster/Scripts/FirstPerson/FirstPersonController.cs
+++ My project/Assets/InventoryMaster/Scripts/FirstPerson/FirstPersonController.cs
@@ -5,15 +5,13 @@ using System.Collections;
 public class FirstPersonController : MonoBehaviour
 {
     private Animator animator;      //....Animator
-    private Rigidbody rigidbody;
-   // public Transform groundCheckerTransform;
-  //  public LayerMask groundLayer;
+    public LayerMask notPlayerMask;
     public float movementspeed = 5.0f;
     public float mouseSensitivity = 2.0f;
     public float verticalAngleLimit = 60.0f;
     public float jumpSpeed = 5f;
 
-    float verticalRotation = 0;
+   // float verticalRotation = 0;
 
     GameObject _inventory;
     GameObject _tooltip;
@@ -33,7 +31,6 @@ public class FirstPersonController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();  //....get animator
-        //rigidbody = GetComponent<Rigidbody>();
        
         firstPersonCamera = Camera.main.GetComponent<Camera>();
         characterController = GetComponent<CharacterController>();
@@ -73,9 +70,10 @@ public class FirstPersonController : MonoBehaviour
             verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
             if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+            if (Input.GetButtonDown("Jump") && characterController.isGrounded)
             {
-                 verticalVelocity = jumpSpeed;
-                //Jump();
+                verticalVelocity = jumpSpeed;
+                
             }
 
             Vector3 speed = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
@@ -86,25 +84,10 @@ public class FirstPersonController : MonoBehaviour
 
             //My rotation
             if (rotationspeed.magnitude > Mathf.Abs(0.05f))
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotationspeed), Time.deltaTime * 10);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotationspeed), Time.deltaTime * 10);
 
             characterController.Move(speed * Time.deltaTime);
-        }
-        /*
-        void Jump()
-        {
-            RaycastHit hit;
-            if(Physics.Raycast(groundCheckerTransform.position, Vector3.down, 10f, groundLayer))
-            {
-                rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
-            }
-            else 
-            {
-                Debug.Log("Did not find ground layer");
-            }
-        }
-        */
-
+        }   
     }
 
 
